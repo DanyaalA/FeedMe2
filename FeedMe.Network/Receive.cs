ed03845a7ac1a296ce82671056a71a9cc346c4f1
DanyaalA <Danny13653@gmail.com>
2020-07-09 16:53:09 +0100

    Created Network Library
    Network Library is a Library Used by both the server and clients to network data between eachother

---
+++ FeedMe.Network/Receive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace FeedMe.Network
+{
+    class Receive
+    {
+        private byte[] ReceiveData(Socket sock)
+        {
+            byte[] buffer = new byte[1024];
+            int size = sock.Receive(buffer);
+            byte[] data = new byte[size];
+            Array.Copy(buffer, data, size);
+
+            return data;
+        }
+
+        public string ReceiveMessage(Socket sock)
+        {
+            return Encoding.UTF8.GetString(ReceiveData(sock));
+        }
+    }
+}
---
+++ FeedMe.Network/Send.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace FeedMe.Network
+{
+    class Send
+    {
+        public void SendData(Socket sock, byte[] data)
+        {
+            sock.Send(data, 0, data.Length, SocketFlags.None);
+        }
+
+        public void SendMessage(Socket sock, string message)
+        {
+            SendData(sock, Encoding.UTF8.GetBytes(message));
+        }
+    }
+}
