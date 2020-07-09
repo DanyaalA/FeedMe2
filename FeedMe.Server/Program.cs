df5582100d2633827fc32e98d91820aa92eaf283
DanyaalA <Danny13653@gmail.com>
2020-07-09 16:52:30 +0100

    Added Server

---
+++ FeedMe.Server/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace FeedMe.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server s = new Server();
+            s.StartServer();
+        }
+    }
+}
---
+++ FeedMe.Server/Server.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace FeedMe.Server
+{
+    class Server
+    {
+        private Socket sSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        int PORT = 4030;
+        string ADDRESS = "127.0.0.1";
+        private IPEndPoint ipe;
+
+        public void StartServer()
+        {
+            ipe = new IPEndPoint(IPAddress.Parse(ADDRESS), PORT);
+            sSock.Bind(ipe);
+
+            sSock.Listen(100);
+
+            Socket clientSock = default;
+
+            Server serverInstance = new Server();
+
+            while (true)
+            {
+                clientSock = sSock.Accept();
+                Thread clientThread = new Thread(() => HandleClient(clientSock));
+            }
+        }
+
+        private void HandleClient(Socket sock)
+        {
+            Console.WriteLine("Running Da Tings");
+        }
+    }
+}
