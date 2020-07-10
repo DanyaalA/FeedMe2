d66a6ce1b8ae3337fbf625ea0d1722cc6b3c565c
DanyaalA <Danny13653@gmail.com>
2020-07-10 23:55:34 +0100

    MySql Data Connection
    MySql Connection isn't finished yet

---
+++ FeedMe.Server/Data/MySqlConnector.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeedMe.Server.Data
+{
+    class MySqlConnector
+    {
+        private string conString = "server=localhost;user=fServer;database=feedme;port=3306;password=1234";
+        private MySqlConnection conn = new MySqlConnection();
+
+        public void InitializeConnection()
+        {
+            conn = new MySqlConnection(conString);
+        }
+
+        public void ExecuteQuery()
+        {
+
+            
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error" + ex.Message);
+            }
+
+            conn.Close();
+        }
+
+        public MySqlCommand CreateQuery(string Query)
+        {
+            return new MySqlCommand(Query, conn);
+        }
+
+        public MySqlCommand AddParamter(MySqlCommand command, string paramName, string value)
+        {
+            command.Parameters.AddWithValue(paramName, value);
+            return command;
+        }
+    }
+}
---
+++ FeedMe.Server/Shared/Authentication/Login.cs
@@ -0,0 +1,22 @@
+﻿
+using FeedMe.Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeedMe.Server.Shared.Authentication
+{
+    class Login
+    {
+        private string username, pword;
+        public Login(Client client)
+        {
+            username = client.ReceiveMessage();
+            pword = client.ReceiveMessage();
+        }
+        public void AuthenticateLogin()
+        {
+
+        }
+    }
+}
