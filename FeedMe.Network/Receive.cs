b93afab03afa6859a3ab692bb1d45ba821a4b204
DanyaalA <Danny13653@gmail.com>
2020-07-13 14:24:51 +0100

    Removed Asynchronous Receiving

--- FeedMe.Network/Receive.cs
+++ FeedMe.Network/Receive.cs
@@ -8,20 +8,20 @@ namespace FeedMe.Network
 {
     public class Receive
     {
-        private async Task<byte[]> ReceiveData(Socket sock)
+        private byte[] ReceiveData(Socket sock)
         {
             byte[] buffer = new byte[1024];
-            int size = await Task.Run(() => sock.Receive(buffer));
+            int size = sock.Receive(buffer);
             byte[] data = new byte[size];
             Array.Copy(buffer, data, size);
 
             return data;
         }
 
-        public async Task<string> ReceiveMessage(Socket sock)
+        public string ReceiveMessage(Socket sock)
         {
 
-            return Encoding.UTF8.GetString(await ReceiveData(sock));
+            return Encoding.UTF8.GetString(ReceiveData(sock));
         }
     }
 }
